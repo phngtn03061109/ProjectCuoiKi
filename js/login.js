2ac28b412775d8bf90d2b6e78320217dcac58474
phngtn03061109 <phung147741@gmail.com>
2022-05-30 19:15:25 +0700

    file change all

--- js/login.js
+++ js/login.js
@@ -1,39 +1,55 @@
-var email = document.forms['form']['email'];
-var password = document.forms['form']['password'];
+var emaill = document.forms['form']['email'];
+var passwordd = document.forms['form']['password'];
 
 var email_error = document.getElementById('email_error');
 var pass_error = document.getElementById('pass_error');
 
-email.addEventListener('textInput', email_Verify);
-password.addEventListener('textInput', pass_Verify);
+emaill.addEventListener('textInput', email_Verify);
+passwordd.addEventListener('textInput', pass_Verify);
 
-function validated(){
-	if (email.value.length < 8) {
-		email.style.border = "1px solid red";
-		email_error.style.display = "block";
-		email.focus();
+function validateddd(){
+	if (emaill.value.length < 8) {
+		emaill.style.border = "1px solid red";
+		emaill_error.style.display = "block";
+		emaill.focus();
 		return false;
 	}
-	if (password.value.length < 6) {
-		password.style.border = "1px solid red";
-		pass_error.style.display = "block";
-		password.focus();
-		return false;
-	}	
 }
 
 function email_Verify(){
-	if (email.value.length >= 8) {
-		email.style.border = "1px solid silver";
-		email_error.style.display = "none";
+	if (emaill.value.length >= 8) {
+		emaill.style.border = "1px solid silver";
+		emaill_error.style.display = "none";
 		return true;
 	}
 }
 
 function pass_Verify(){
-	if (password.value.length >= 6) {
-		password.style.border = "1px solid silver";
+	if (passwordd.value.length >= 6) {
+		passwordd.style.border = "1px solid silver";
 		pass_error.style.display = "none";
 		return true;
 	}
+}
+var attempt = 3; // Variable to count number of attempts.
+// Below function Executes on click of login button.
+function validate(){
+var email = document.getElementById("email").value;
+var password = document.getElementById("password").value;
+if ( email == "phungtn" && password == "conginuadau222"){
+alert ("Login successfully");
+window.location.href("index.html"); // Redirecting to other page.
+return false;
+}
+else{
+attempt --;// Decrementing by one.
+alert("You done write fail password;");
+// Disabling fields after 3 attempts.
+if( attempt == 0){
+document.getElementById("email").disabled = true;
+document.getElementById("password").disabled = true;
+document.getElementById("submit").disabled = true;
+return false;
+}
+}
 }
